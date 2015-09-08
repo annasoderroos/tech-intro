93da411b8e9ef8b5b4c1ff2713e3427599ef1e99
annasoderroos <anna.soderroos@gmail.com>
2015-09-08 08:57:56 +0200

    complete tech-intro

---
+++ public/assets/js/app.js
@@ -0,0 +1,15 @@
+jQuery(document).ready(function() {
+    jQuery('#liquid').liquidcarousel({
+        height: 300
+    });
+});
+jQuery('#ourButton').on('click', function () {
+    jQuery.ajax({
+        url: '/animal_list_data'
+    }).done(function (data) {
+        console.log('fetched data', data.animals);
+        data.animals.forEach(function (item) {
+            jQuery('.fetchedItems').append('<li><img src="' + item + '" width="60" /></li>');
+        });
+    });
+});
